cdf3d0864f9efaac75c48088b2ef67191e2f86b2
gfcodearq <69319238+gfcodearq@users.noreply.github.com>
2021-08-17 13:42:36 -0300

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+const char Asterisco = '*';
+int main(int argc, char *argv[]) {
+	int n;
+	cout<<"Ingrese un numero por teclado: "<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++) //desde que i es =o hasta i<n  - 0/1
+	{
+		for(int j=0;j<=i;j++) //desde j=0 hasta j < i - 0/1 siempre iguala a i entonces si es igual a 2 va a imprimir 3 veces antes de cerrar el bucle
+		{
+			cout<<Asterisco;		
+		}
+		cout<<endl;		
+	}		
+	return 0;
+}
+
+
+/* Dise ~ne un programa que lea un n ´umero entero de teclado y escriba un tri ´angulo rect ´angulo (relleno) con
+	tantos asteriscos (*) de base y altura como indique el n ´umero le ´ido. Por ejemplo, para un n ´umero le ´ido
+	con valor 5, escribir ´a:
+	*
+	**
+	***
+	****
+	*****
+*/
---
+++ 23.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+const char Asterisco = '*';
+int main(int argc, char *argv[]) {
+	int n;
+	cout<<"Ingrese un numero por teclado: "<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++) //desde que i es =o hasta i<n  - 0/1
+	{
+		for(int j=0;j<=i;j++) //desde j=0 hasta j < i - 0/1 siempre iguala a i entonces si es igual a 2 va a imprimir 3 veces antes de cerrar el bucle
+		{
+			if((i==0)|| (j==0) || (i == 1) || (i == n))
+			{
+				cout<<Asterisco;
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<endl;		
+	}		
+	return 0;
+}
+
+
+/* Dise ~ne un programa que lea un n ´umero entero de teclado y escriba un tri ´angulo rect ´angulo (relleno) con
+	tantos asteriscos (*) de base y altura como indique el n ´umero le ´ido. Por ejemplo, para un n ´umero le ´ido
+	con valor 5, escribir ´a:
+	*
+	**
+	* *
+	*  *
+	*****
+*/
