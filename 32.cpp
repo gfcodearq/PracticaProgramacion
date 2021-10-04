0133aa72a820b3836e68f2179b4ca1fb8bcbcd7b
gfcodearq <69319238+gfcodearq@users.noreply.github.com>
2021-10-04 10:49:04 -0300

    Add files via upload

---
+++ 32.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	
+	int n;	
+	cout<<"Ingrese un numero: "<<endl;
+	cin>>n;	
+	
+	if(n>1) // un numero primo es mayor que 1
+	{
+		if(n%n==0 && n%1==0)
+		{						
+			cout<<"Es verdadero"<<endl;
+		}
+		else 
+	    {
+			cout<<"No es primo"<<endl;
+		}
+	}
+	else
+	{
+		cout<<"El numero tiene que ser mayor que 1"<<endl;
+	}
+		
+	
+	return 0;
+}
+
+//si es numero primo es true sino es false 
---
+++ 33.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	int contComa=0;	
+	int contCaracteres=0;
+	char miCadena;
+	char coma=',';
+	//char punto='.';
+	
+	cout<<"Escriba lo que desee: ";
+	cin.get(miCadena);
+	
+	while (miCadena!='.')
+	{
+		contCaracteres++;
+		if(miCadena==coma)
+		{
+		contComa++;
+		}	
+		cin.get(miCadena);
+	}
+	
+		cout<<"La cantidad de comas es: "<<contComa<<endl;
+		cout<<"La cantidad de caracteres es: "<<contCaracteres<<endl;
+		
+	return 0;
+}
+
